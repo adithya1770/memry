84faa506f32f0172cf1385966015725569f3c60a
adithya1770 <adithyaps929@gmail.com>
2025-09-25 21:58:08 +0530

    added p5js

--- web/sketch.js
+++ web/sketch.js
@@ -1,9 +1,13 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1535, 695);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  rect(100, 100, 340, 200);
   textSize(24);
-  text("sup?", 50, 200);
+  fill(255);
+  text("RAM", 100, 100);
+  stroke(255);
+  line(270, 300, 270, 400);
 }
